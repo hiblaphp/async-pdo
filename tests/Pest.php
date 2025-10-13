710e9ef89e4a8fdea3a45e79c468f059620d0529
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-10-13 15:03:03 +0800

    update teest

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,5 +4,8 @@ pest()->extend(Tests\TestCase::class)->in('Feature');
 pest()->extend(Tests\TestCase::class)->in('Unit');
 pest()->extend(Tests\TestCase::class)->in('Integration');
 
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+// $dotenv->load();
+
 
 
